3f140a70ff2918b676c81151a6af48712e050b22
Jonas Svensson <jonas.m.svensson@gmail.com>
2024-04-15 18:53:26 +0200

    Update type

--- app/languages.ts
+++ app/languages.ts
@@ -7,5 +7,4 @@ const dictionaries = {
 
 export const getDictionary = async (
   locale: string,
-): Promise<(category: string) => Record<string, string>> =>
-  dictionaries[locale]();
+): Promise<Record<string, Record<string, string>>> => dictionaries[locale]();
