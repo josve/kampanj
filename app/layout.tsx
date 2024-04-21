f3f4df901e04381f21dbd2d70e0f7ce399bb9dfa
Jonas Svensson <jonas.m.svensson@gmail.com>
2024-04-21 13:43:50 +0200

    Add support for custom fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,30 @@ export default function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
+  const cssText = `
+  :root {
+  --header-font-family: ${process.env.NEXT_PUBLIC_HEADER_FONT_FAMILY};
+  --content-font-family: ${process.env.NEXT_PUBLIC_CONTENT_FONT_FAMILY};
+  }
+  @font-face {
+    font-family: ${process.env.NEXT_PUBLIC_HEADER_FONT_FAMILY};
+    src: url('${process.env.NEXT_PUBLIC_HEADER_FONT_URL}');
+    font-style: normal;
+    font-weight: bold;
+    text-rendering: optimizeLegibility;
+  }
+  @font-face {
+    font-family: ${process.env.NEXT_PUBLIC_CONTENT_FONT_FAMILY};
+    src: url('${process.env.NEXT_PUBLIC_CONTENT_FONT_URL}');
+    font-style: normal;
+    text-rendering: optimizeLegibility;
+  }
+`;
+
   return (
     <html lang={locale}>
       <head>
+        <style dangerouslySetInnerHTML={{ __html: cssText }} />
         <meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
       </head>
 
